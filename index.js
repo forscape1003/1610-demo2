ef08045f83d74c1eadb1d55987df38c2d8c09986
Tian Dong xue <forscape@163.com>
2017-02-16 16:36:53 +0800

    add demo2

---
+++ index.js
@@ -0,0 +1,8 @@
+var cc = require('./cc')
+
+
+function a(x,y){
+    return x+y+cc;
+}
+
+module.exports = a;
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry:'./demo.js',
+    output:{
+        path:'build',
+        filename:'bundle.js'
+    },
+    devtool:'eval'
+};
